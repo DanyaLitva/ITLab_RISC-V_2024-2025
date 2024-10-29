9ec923075ee7fc63cc483d295405b11b3f6aa5b3
Даня <dattesuki@mail.ru>
2024-10-29 13:05:03 +0300

    FP16 v0.18

--- FP16/FP16.cpp
+++ FP16/FP16.cpp
@@ -9,14 +9,22 @@
 const int signLength = 1;
 const int expLength = 5;
 const int manLength = 10;
-
+/*
 typedef struct {
     unsigned int sign : 1; //знак           %2
     unsigned int exp : 5; //порядок         %32
     unsigned int man : 10; //мантисса       %1024
     float ref; //референс для сравнения
 } FP16;
-
+*/
+typedef union {
+    struct{
+        unsigned int man : 10; //мантисса       %1024
+        unsigned int exp : 5; //порядок         %32
+        unsigned int sign : 1; //знак           %2
+    } in;
+    float ref; //референс для сравнения
+} FP16;
 int GetSignFP16(FP16 A);
 int GetExpFP16(FP16 A);
 int GetManFP16(FP16 A);
@@ -49,18 +57,18 @@ int main()
     FP16 A,B,C,D;
     float f1, f2, f3;
     
-    A.sign = 0;
-    A.exp = 17;
-    A.man = 25;
+    A.in.sign = 0;
+    A.in.exp = 17;
+    A.in.man = 25;
     
-    f1 = -17.05;
+    f1 = -1.55f;
     ConvertftoFP16(f1, &A);
     
-    B.sign = 0;
-    B.exp = 16;
-    B.man = 700;
+    B.in.sign = 0;
+    B.in.exp = 16;
+    B.in.man = 700;
     
-    f2=-19.01;
+    f2=-1.61f;
     ConvertftoFP16(f2, &B);
 
     AddFP16_2(A, B, &C);
@@ -81,16 +89,16 @@ int main()
 
 
     printf("\n");
-    A.sign = 0;
-    A.exp = 17;
-    A.man = 25;
+    A.in.sign = 0;
+    A.in.exp = 17;
+    A.in.man = 25;
     
     f1 = 150.0f;
     ConvertftoFP16(f1, &A);
     
-    B.sign = 0;
-    B.exp = 16;
-    B.man = 700;
+    B.in.sign = 0;
+    B.in.exp = 16;
+    B.in.man = 700;
     
     f2=300.0f;
     ConvertftoFP16(f2, &B);
@@ -121,62 +129,62 @@ int main()
 
 
 
-int GetSignFP16(FP16 A) { return A.sign; }
+int GetSignFP16(FP16 A) { return A.in.sign; }
 
-int GetExpFP16(FP16 A) { return A.exp; }
+int GetExpFP16(FP16 A) { return A.in.exp; }
 
-int GetManFP16(FP16 A) { return A.man; }
+int GetManFP16(FP16 A) { return A.in.man; }
 
 float GetRefFP16(FP16 N) { return N.ref; }
 
 //operator=
 void AssignFP16(FP16* N1, FP16 N2) {
-    N1->sign = N2.sign;
-    N1->exp = N2.exp;
-    N1->man = N2.man;
+    N1->in.sign = N2.in.sign;
+    N1->in.exp = N2.in.exp;
+    N1->in.man = N2.in.man;
     N1->ref = N2.ref;
 }
 
 
 void ConvertFP16tof(FP16 N, float* f) {
     int shiftExp = (1 << (expLength - 1)) - 1; //смещение   -15 ... 16
-    if (N.sign == 1) (*f *= -1);
+    if (N.in.sign == 1) (*f *= -1);
 
     float temp = 1.0f;
     float temp2 = 1.0f;
     for (int i = 0; i < manLength; ++i) {
         temp2 /= 2.0f;
-        if ((N.man & (1 << (manLength - i - 1))) != 0)   temp += temp2;
+        if ((N.in.man & (1 << (manLength - i - 1))) != 0)   temp += temp2;
     }
-    printf("%d\n", N.exp - shiftExp);
-    if (N.exp >= shiftExp) *f = pow(-1,N.sign)*(float)(pow(2, (N.exp - shiftExp))) * temp;
-    else *f = pow(-1,N.sign)*(float)(1/(pow(2, (shiftExp - N.exp)))) * temp;
+    //printf("%d\n", N.in.exp - shiftExp);
+    if (N.in.exp >= shiftExp) *f = pow(-1,N.in.sign)*(float)(pow(2, (N.in.exp - shiftExp))) * temp;
+    else *f = pow(-1,N.in.sign)*(float)(1/(pow(2, (shiftExp - N.in.exp)))) * temp;
 }
 
 
 void PrintFP16(FP16 N) {
     int temp;
-    printf("%c", (N.sign + '0'));
+    printf("%c", (N.in.sign + '0'));
     for (int i = 0; i < expLength; ++i) {
-        if ((N.exp & (1 << (expLength - i - 1))) != 0) temp = 1; else temp = 0;
+        if ((N.in.exp & (1 << (expLength - i - 1))) != 0) temp = 1; else temp = 0;
         printf("%c", (temp + '0'));
     }
     for (int i = 0; i < manLength; ++i) {
-        if ((N.man & (1 << (manLength - i - 1))) != 0) temp = 1; else temp = 0;
+        if ((N.in.man & (1 << (manLength - i - 1))) != 0) temp = 1; else temp = 0;
         printf("%c", (temp + '0'));
     }
 }
 
 void PrintFP16_ed(FP16 N) {
     int temp;
-    printf("%c_", (N.sign + '0'));
+    printf("%c_", (N.in.sign + '0'));
     for (int i = 0; i < expLength; ++i) {
-        if ((N.exp & (1 << (expLength - i-1))) != 0) temp = 1; else temp = 0;
+        if ((N.in.exp & (1 << (expLength - i-1))) != 0) temp = 1; else temp = 0;
         printf("%c", (temp + '0'));
     }
     printf("_");
     for (int i = 0; i < manLength; ++i) {
-        if ((N.man & (1 << (manLength - i-1))) != 0) temp = 1; else temp = 0;
+        if ((N.in.man & (1 << (manLength - i-1))) != 0) temp = 1; else temp = 0;
         printf("%c", (temp + '0'));
     }
 }
@@ -189,48 +197,47 @@ void PrintFP16_f(FP16 N) {
 
 
 void AddFP16(FP16 N1, FP16 N2, FP16* Res) {
-    int diff = abs(N1.exp - N2.exp); //разница порядков
+    int diff = abs(N1.in.exp - N2.in.exp); //разница порядков
     int MaxExp;
-    if (N1.exp >= N2.exp) MaxExp = N1.exp;
-    else MaxExp = N2.exp;
+    if (N1.in.exp >= N2.in.exp) MaxExp = N1.in.exp;
+    else MaxExp = N2.in.exp;
     int temp;
 
     int shift;
     if ((manLength - diff) <= 0) shift = 0;
     else shift = (1 << (manLength - diff));
 
-    temp = (N1.man >> (MaxExp - N1.exp)) + (N2.man >> (MaxExp - N2.exp)) + shift + (1 << manLength);
+    temp = (N1.in.man >> (MaxExp - N1.in.exp)) + (N2.in.man >> (MaxExp - N2.in.exp)) + shift + (1 << manLength);
     if (temp >= (1 << (manLength+1))) {
        temp = (temp / 2);
        MaxExp++;
     }
-    Res->exp = MaxExp;
-    Res->man = (temp - (1<<manLength));
+    Res->in.exp = MaxExp;
+    Res->in.man = (temp - (1<<manLength));
 }
 
 void AddFP16_2(FP16 N1, FP16 N2, FP16* Res) {
-    int diff = abs(N1.exp - N2.exp); //разница порядков
+    int diff = abs(N1.in.exp - N2.in.exp); //разница порядков
     int MaxExp;
     int sign_Min,sign_Max;
-    if (N1.exp >= N2.exp) {
-        MaxExp = N1.exp;
-        sign_Min=N2.sign;
-        sign_Max=N1.sign;
+    if (N1.in.exp >= N2.in.exp) {
+        MaxExp = N1.in.exp;
+        sign_Min=N2.in.sign;
+        sign_Max=N1.in.sign;
     }
     else {
-        MaxExp = N2.exp;
-        sign_Min=N1.sign;
-        sign_Max=N2.sign;
+        MaxExp = N2.in.exp;
+        sign_Min=N1.in.sign;
+        sign_Max=N2.in.sign;
     }
     int temp;
     
     int shift;
     if ((manLength - diff) <= 0) shift = 0;
     else shift = (1 << (manLength - diff));
-
-    temp = pow(-1,N1.sign)*(N1.man >> (MaxExp - N1.exp)) + pow(-1,N2.sign)*(N2.man >> (MaxExp - N2.exp)) + pow(-1,sign_Min)*shift + pow(-1,sign_Max)*(1 << manLength);
-    if (temp<0) Res->sign = 1;
-    else Res->sign = 0;
+    temp = pow(-1,N1.in.sign)*(N1.in.man >> (MaxExp - N1.in.exp)) + pow(-1,N2.in.sign)*(N2.in.man >> (MaxExp - N2.in.exp)) + pow(-1,sign_Min)*shift + pow(-1,sign_Max)*(1 << manLength);
+    if (temp<0) Res->in.sign = 1;
+    else Res->in.sign = 0;
     temp=abs(temp);
     while(temp<(1<<manLength)){
         temp*=2;
@@ -240,18 +247,18 @@ void AddFP16_2(FP16 N1, FP16 N2, FP16* Res) {
        temp = (temp / 2);
        MaxExp++;
     }
-    Res->exp = MaxExp;
-    Res->man = (temp - (1<<manLength));
+    Res->in.exp = MaxExp;
+    Res->in.man = (temp - (1<<manLength));
 
 }
 
 
 void MulFP16(FP16 N1, FP16 N2, FP16* Res) {
     int shiftExp = (1 << (expLength - 1)) - 1; //смещение   -15 ... 16
-    int temp2 = ((N1.man * N2.man)/(1<<manLength)); //сохранять, это последнийи бит разложить на / и %
-    int temp = ((N1.man * N2.man)/(1<<manLength)) + N1.man + N2.man  + (1<<manLength);
+    int temp2 = (N1.in.man * N2.in.man); //сохранять, тут последниий бит разложить на / и %
+    int temp = ((N1.in.man * N2.in.man)/(1<<manLength)) + N1.in.man + N2.in.man  + (1<<manLength);
 
-    Res->exp = ((N1.exp-shiftExp) + (N2.exp-shiftExp) + shiftExp);
+    Res->in.exp = ((N1.in.exp-shiftExp) + (N2.in.exp-shiftExp) + shiftExp);
     //int temp2 = (1 << manLength);
     /*while (temp >= (1<<(manLength))) {
         temp = (temp / 2);
@@ -261,15 +268,15 @@ void MulFP16(FP16 N1, FP16 N2, FP16* Res) {
     }*/
     if(temp>= + (1<<(manLength+1))) {
         temp/=2; //и здесь один бит теряется
-        Res->exp++;
+        Res->in.exp++;
     }
-    Res->man = (temp);
-    Res->sign = N1.sign + N2.sign;
+    Res->in.man = (temp);
+    Res->in.sign = N1.in.sign + N2.in.sign;
 }
-
-
 //прибавить 1024 к temp
 //если больше 2048 делим все на пополам
+
+
 char bytes[sizeof(float)];
 char bites[sizeof(float) * 8];
 //float: 1:8:23
@@ -284,17 +291,18 @@ void ConvertftoFP16(float f, FP16* N){
         //if(bites[31-i]!=0)    printf("%d = %d ", (31-i),bites[31-i]);
     }
     
-    N->sign = bites[0];
-    N->exp = bites[1] * (1 << (expLength-1));
-    N->man = 0;
+    N->in.sign = bites[0];
+    N->in.exp = bites[1] * (1 << (expLength-1));
+    N->in.man = 0;
     for (int i = 0; i < 4; ++i) {
-        N->exp = N->exp + (bites[5+i] * (1 << (expLength - 2 - i)));
+        N->in.exp = N->in.exp + (bites[5+i] * (1 << (expLength - 2 - i)));
     }
     for (int i = 0; i < 10; ++i) {
-        N->man = N->man + (bites[9 + i] * (1 << (manLength - 1 - i)));
+        N->in.man = N->in.man + (bites[9 + i] * (1 << (manLength - 1 - i)));
     }
     //PrintFP16_ed(*N);
 }
+
 
 float GetFloat(FP16 N){
     float temp;
@@ -307,10 +315,7 @@ float GetFloat(FP16 N){
 
 
 
-
-
-
-
+/*
 void Test(){
     //
     int flag = 0;
@@ -321,9 +326,9 @@ void Test(){
         for(int _man = 0; _man<1024;_man++){
             if (flag!=1){
                 //A.sign=B.sign=_sign;
-                A.sign=B.sign=0;
-                A.exp=B.exp=_exp;
-                A.man=B.man=_man;
+                A.in.sign=B.in.sign=0;
+                A.in.exp=B.in.exp=_exp;
+                A.in.man=B.in.man=_man;
                 
                 MulFP16(A, B, &C);
                 ConvertFP16tof(A, &f1);
@@ -346,3 +351,11 @@ void Test(){
 }
 
 
+*/
+/*
+//сабнормал + нормал
+если эксп позволяет, то сдвигаем мантиссу
+ если эксп 0, оставляем мантиссу как есть
+
+
+*/
