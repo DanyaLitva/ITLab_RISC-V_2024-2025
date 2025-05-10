e3bf9ef93339047967e7709f4d93c4d5c793fcb6
dattesuki <dattesuki@mail.ru>
2025-05-11 00:41:10 +0300

    Reapply "fp16 update"
    This reverts commit f7a9f7d9d118e056cb3660ed689b02986ca43000.

--- GaussSeidel/FP16.h
+++ GaussSeidel/FP16.h
@@ -3,17 +3,17 @@
 #include <cstring>
 #include <cmath>
 #pragma once
-const int manLength = 10;
-const int expLength = 5;
-const int signLength = 1;
-const int shiftExp = (1 << (expLength - 1)) - 1;
-
+//const int manLength = 10;
+//const int expLength = 5;
+//const int signLength = 1;
+//const int shiftExp = (1 << (expLength - 1)) - 1;
+//const int shiftExp = 15;
 
 class FP16 {
 private:
-    unsigned int man : manLength;
-    unsigned int exp : expLength;
-    unsigned int sign : signLength;
+    unsigned int man : 10;
+    unsigned int exp : 5;
+    unsigned int sign : 1;
 
 public:
     FP16(int _sign = 0, int _exp = 0, int _man = 0):sign(_sign), exp(_exp), man(_man) {}
@@ -25,18 +25,18 @@ public:
     void printFP16_bites() const noexcept;
     //operator float16_t() const noexcept;
     FP16& operator=(const FP16& N) noexcept;
-    void set_from_uint(uint16_t temp) noexcept;
+    void set_from_uint(const uint16_t& temp) noexcept;
     uint16_t get_int() const noexcept;
     //FP16(uint16_t temp = 0) noexcept;
-    FP16 operator+(FP16 right) const noexcept;
+    FP16 operator+(const FP16& right) const noexcept;
     FP16 operator-(FP16 right) const noexcept;
-    FP16 operator*(FP16 right) const noexcept;
-    friend uint16_t mul_half(uint16_t a, uint16_t b) noexcept;
+    FP16 operator*(const FP16& right) const noexcept;
+    friend uint16_t mul_half(const uint16_t& a, const uint16_t& b) noexcept;
     friend uint16_t usub_half(uint16_t temp) noexcept;
-    friend FP16 fma4(FP16 a, FP16 b, FP16 c) noexcept;
-    friend uint16_t fma_half(uint16_t a, uint16_t b, uint16_t c) noexcept;
+    friend FP16 fma4(const FP16& a, const FP16& b, const FP16& c) noexcept;
+    friend uint16_t fma_half(const uint16_t& a, const uint16_t& b, const uint16_t& c) noexcept;
     friend std::ostream& operator<<(std::ostream& os, FP16 num) noexcept;
-    FP16 operator/(FP16 right) const noexcept;
+    FP16 operator/(const FP16& right) const noexcept;
     bool operator == (const FP16& right) const noexcept;
     bool operator != (const FP16& right) const noexcept;
     bool operator < (const FP16& right) const noexcept;
@@ -48,12 +48,12 @@ public:
     FP16& operator*=(const FP16& right) noexcept;
     FP16& operator/=(const FP16& right) noexcept;
     FP16 operator-() noexcept;
-    FP16& operator=(float N) noexcept;
-    FP16& operator=(double N) noexcept;
-    FP16(float num) noexcept{
+    FP16& operator=(const float& N) noexcept;
+    FP16& operator=(const double& N) noexcept;
+    FP16(const float& num) noexcept{
         *this = num;
     }
-    FP16(double num) noexcept{
+    FP16(const double& num) noexcept{
         *this = num;
     }
     FP16& fma(const FP16& a, const FP16& b){
@@ -65,5 +65,5 @@ public:
 };
 
 
-float fp16_to_float(uint16_t h) noexcept;
+float fp16_to_float(const uint16_t& h) noexcept;
 std::ostream& operator<<(std::ostream& os, FP16 num) noexcept;
